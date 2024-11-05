b23200af5070d46d7f299db36e210e09fc43fa29
PytsonKoks <patrykj77@spoko.pl>
2024-11-05 08:16:20 +0100

    dodanie formularza

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className="d-flex flex-column w-100 align-items-center justify-content-center">
+            <div className="form-group">
+                <label htmlFor="email" className="form-label">E-mail</label>
+                <input type="email" className="form-control" id="email" name="email" ></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" name="password"></input>
+            </div>
+            <button type="submit" className="btn btn-primary mt-3">Login</button>
+        </form>
     </>
   )
 }
